5c12468c444b15f9bdf080a1e5cb1ef5718d03fa
garzuzo <garzuxo@gmail.com>
2018-10-28 23:05:15 -0500

    added validations of Tmio1Ruta and Tmio1Bus

--- CI 18-2 MiniProy2 - Inicial/src/main/java/co/edu/icesi/mio/logic/TmioBusesLogic.java
+++ CI 18-2 MiniProy2 - Inicial/src/main/java/co/edu/icesi/mio/logic/TmioBusesLogic.java
@@ -10,47 +10,78 @@ import org.springframework.beans.factory.annotation.Autowired;
 import co.edu.icesi.mio.dao.ITmio1_Buses_DAO;
 import co.edu.icesi.mio.model.Tmio1Bus;
 
-public class TmioBusesLogic implements ITmioBusesLogic{
+public class TmioBusesLogic implements ITmioBusesLogic {
 
 	@Autowired
 	private ITmio1_Buses_DAO buses;
-	
+
 	@PersistenceContext
 	private EntityManager em;
+
 	@Override
 	public void add(Tmio1Bus bus) {
-		// TODO Auto-generated method stub
-		buses.save(em, bus);
+
+		if (placaMESix(bus) && marcaMEThree(bus) && modeloNumbersEFour(bus) && tipoPAT(bus) && capacidadMZero(bus))
+			buses.save(em, bus);
 	}
 
 	@Override
 	public void update(Tmio1Bus bus) {
-		// TODO Auto-generated method stub
-		buses.update(em, bus);
+		if (placaMESix(bus) && marcaMEThree(bus) && modeloNumbersEFour(bus) && tipoPAT(bus) && capacidadMZero(bus))
+			buses.update(em, bus);
 	}
 
 	@Override
 	public void delete(Tmio1Bus bus) {
-		// TODO Auto-generated method stub
+
 		buses.delete(em, bus);
 	}
 
 	@Override
 	public void findByModelo(BigDecimal m) {
-		// TODO Auto-generated method stub
+
 		buses.findByModel(em, m);
 	}
 
 	@Override
 	public void findByTipo(String t) {
-		// TODO Auto-generated method stub
+
 		buses.findByType(em, t);
 	}
 
 	@Override
 	public void findByCapacidad(BigDecimal c) {
-		// TODO Auto-generated method stub
+
 		buses.findByCapacity(em, c);
+	}
+
+	/**
+	 * VALIDACIONES
+	 */
+
+	// la placa esté definida y tenga seis caracteres
+	public boolean placaMESix(Tmio1Bus bus) {
+		return bus.getPlaca().length() >= 6;
+	}
+
+	// la marca esté definida y tenga al menos tres caracteres
+	public boolean marcaMEThree(Tmio1Bus bus) {
+		return bus.getMarca().length() >= 3;
+	}
+
+	// el modelo sea numérico de cuatro dígitos
+	public boolean modeloNumbersEFour(Tmio1Bus bus) {
+		return bus.getModelo().compareTo(new BigDecimal("10000")) < 0;
+	}
+
+	// el tipo sea P, A, o T
+	public boolean tipoPAT(Tmio1Bus bus) {
+		return bus.getTipo().matches("P|A|T");
+	}
+
+	// la capacidad sea numérica mayor a cero
+	public boolean capacidadMZero(Tmio1Bus bus) {
+		return bus.getCapacidad().compareTo(BigDecimal.ZERO) > 0;
 	}
 
 }
--- CI 18-2 MiniProy2 - Inicial/src/main/java/co/edu/icesi/mio/logic/TmioRutasLogic.java
+++ CI 18-2 MiniProy2 - Inicial/src/main/java/co/edu/icesi/mio/logic/TmioRutasLogic.java
@@ -10,36 +10,80 @@ import org.springframework.beans.factory.annotation.Autowired;
 import co.edu.icesi.mio.dao.ITmio1_Rutas_DAO;
 import co.edu.icesi.mio.model.Tmio1Ruta;
 
-public class TmioRutasLogic implements ITmioRutasLogic{
+public class TmioRutasLogic implements ITmioRutasLogic {
 
-	
 	@Autowired
 	private ITmio1_Rutas_DAO rutas;
-	
+
 	@PersistenceContext
 	private EntityManager em;
+
 	@Override
 	public void add(Tmio1Ruta ruta) {
-		// TODO Auto-generated method stub
-		rutas.save(em, ruta);
+	
+		if (validacionNumeroRuta(ruta) && validacionDiaInicio(ruta) && validacionDiaFin(ruta)
+				&& validacionDiaInicioMenorFin(ruta) && validacionHoraFin(ruta) && validacionHoraInicio(ruta)
+				&& validacionHoraInicioMenorFin(ruta) && validacionActiva(ruta))
+			rutas.save(em, ruta);
 	}
 
 	@Override
 	public void update(Tmio1Ruta ruta) {
-		// TODO Auto-generated method stub
+		if (validacionNumeroRuta(ruta) && validacionDiaInicio(ruta) && validacionDiaFin(ruta)
+				&& validacionDiaInicioMenorFin(ruta) && validacionHoraFin(ruta) && validacionHoraInicio(ruta)
+				&& validacionHoraInicioMenorFin(ruta) && validacionActiva(ruta))
 		rutas.update(em, ruta);
 	}
 
 	@Override
 	public void delete(Tmio1Ruta ruta) {
-		// TODO Auto-generated method stub
+
 		rutas.delete(em, ruta);
 	}
 
 	@Override
-	public void findByRangoDias(BigDecimal di,BigDecimal df) {
+	public void findByRangoDias(BigDecimal di, BigDecimal df) {
 		// TODO Auto-generated method stub
 		rutas.findByRangeOfDays(em, di, df);
+	}
+
+	/**
+	 * VALIDACIONES
+	 */
+
+	public boolean validacionNumeroRuta(Tmio1Ruta ruta) {
+		return ruta.getNumero().length() == 3;
+	}
+
+	public boolean validacionDiaInicio(Tmio1Ruta ruta) {
+		return ruta.getDiaInicio().compareTo(BigDecimal.ONE) >= 0
+				&& ruta.getDiaInicio().compareTo(new BigDecimal("7")) <= 0;
+	}
+
+	public boolean validacionDiaFin(Tmio1Ruta ruta) {
+		return ruta.getDiaFin().compareTo(BigDecimal.ONE) >= 0 && ruta.getDiaFin().compareTo(new BigDecimal("7")) <= 0;
+	}
+
+	public boolean validacionDiaInicioMenorFin(Tmio1Ruta ruta) {
+		return ruta.getDiaInicio().compareTo(ruta.getDiaFin()) <= 0;
+	}
+
+	public boolean validacionHoraFin(Tmio1Ruta ruta) {
+		return ruta.getHoraFin().compareTo(BigDecimal.ONE) >= 1
+				&& ruta.getHoraFin().compareTo(new BigDecimal("1440")) <= 0;
+	}
+
+	public boolean validacionHoraInicio(Tmio1Ruta ruta) {
+		return ruta.getHoraInicio().compareTo(BigDecimal.ONE) >= 1
+				&& ruta.getHoraInicio().compareTo(new BigDecimal("1440")) <= 0;
+	}
+
+	public boolean validacionHoraInicioMenorFin(Tmio1Ruta ruta) {
+		return ruta.getHoraInicio().compareTo(ruta.getHoraFin()) <= 0;
+	}
+
+	public boolean validacionActiva(Tmio1Ruta ruta) {
+		return ruta.getActiva().matches("S|N");
 	}
 
 }
